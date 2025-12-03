0b43a7eeb0bb641ba84a9ceea25b90ea087f8d42
aariprom <jseokp4@gmail.com>
2025-12-04 06:14:32 +0900

    [Others] - update: version up

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -41,8 +41,8 @@ android {
         applicationId = "com.ee309.detectivegame"
         minSdk = 24
         targetSdk = 36
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "1.1"
 
         // testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         // override above by hilt test runner
